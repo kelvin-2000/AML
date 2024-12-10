8f7637e3a12a30159063868cbef029d62a1fa7a8
AyeMyatLwin <108206521+ayemyatlwin@users.noreply.github.com>
2024-12-10 13:28:30 +0630

    feat : frontend setup, media list, borrow form, request form

---
+++ frontend/src/app/return/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReturnForm from "@/app/my-components/return-form";
+
+export default function ReturnPage() {
+  return (
+    <main className=" bg-[#f3e9dc]/80">
+      <div className="container mx-auto px-4 max-w-2xl">
+        <ReturnForm />
+      </div>
+    </main>
+  )
+}
+
